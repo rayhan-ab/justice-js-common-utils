174ef10a584abbf63a392b921cce36f88642483b
yanuar <yanuar@accelbyte.net>
2019-11-14 13:58:03 +0700

    PUBP-1654 Add ecommerce service error translation, add permanent type error translation

---
+++ src/lib/service-error-translator/error-translation-map/ecommerce-error-translation-map.tsx
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019. AccelByte Inc. All Rights Reserved
+ * This is licensed software from AccelByte Inc, for limitations
+ * and restrictions contact your company contract manager.
+ */
+
+import * as React from "react";
+import { Trans } from "react-i18next";
+
+export const EcommerceErrorTranslationMap: { [key: string]: React.ReactNode } = {
+  38171: <Trans i18nKey="serviceError.38171">Sorry, you already own this item</Trans>,
+};
