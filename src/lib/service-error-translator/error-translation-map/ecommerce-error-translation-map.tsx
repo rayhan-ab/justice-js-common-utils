c2c6900751c2513502044702b19988789fa0f4d5
fursandy <fursandy@accelbyte.net>
2020-06-11 15:27:18 +0700

    CORE-127 Added error cloning store translation

--- src/lib/service-error-translator/error-translation-map/ecommerce-error-translation-map.tsx
+++ src/lib/service-error-translator/error-translation-map/ecommerce-error-translation-map.tsx
@@ -9,4 +9,5 @@ import { Trans } from "react-i18next";
 
 export const EcommerceErrorTranslationMap: { [key: string]: React.ReactNode } = {
   38171: <Trans i18nKey="serviceError.38171">Sorry, you already own this item</Trans>,
+  30122: <Trans i18nKey="serviceError.30122">Store's meta mismatch</Trans>
 };
